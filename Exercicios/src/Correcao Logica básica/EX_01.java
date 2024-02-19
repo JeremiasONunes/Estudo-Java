139f18e40d6cc63d6acd178348b8bd58e3296617
Jeremias De Oliveira Nunes <Jeremias.6292@gmail.com>
2024-02-19 16:21:54 -0300

    update
    update

--- Exercicios/src/Correcao Logica básica/EX_01.java
+++ Exercicios/src/Correcao Logica básica/EX_01.java
@@ -1,6 +1,6 @@
 package correcao;
 
-public class Ex_01 {
+public class EX_01 {
 /*
  * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
 mensagem explicativa, conforme exemplos.
